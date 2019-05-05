e3ef0ebee5af7709265a536161df063ef52ebb8f
Anthony SOTTEJEAU <anthony@projet.io>
2019-05-05 17:00:45 +0200

    new exemple

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-let test = new autocomplete('autocomplete');
+let input1 = new autocomplete('autocomplete');
 
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(response => response.json())
 .then(data => {
     console.log(data);
-  test.setData(data.map(function (item) {
+  input1.setData(data.map(function (item) {
     return item.name;
   }));
 });
+
+let input2 = new autocomplete('autocomplete');
+input2.setData(['France', 'Allemagne', 'Angleterre', 'Autriche', 'Espagne', 'Italie', 'Pologne', 'Suede', 'Croatie', 'Belgique', 'Suisse']);
