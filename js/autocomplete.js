a772e2d41cb8a5f2e60b161c0a74bcdb05e13d74
Anthony SOTTEJEAU <anthony@projet.io>
2019-05-05 19:12:16 +0200

    done

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,27 +1,28 @@
 class autocomplete {
 
-    constructor(element, minLetter = 3, filter = false, order = 'none') {
+    constructor(element, minLetter = 3, filter = false, order = 'none', highlight = true) {
         this.input = document.getElementById(element);
         this.minLetter = minLetter;
         this.filter = filter;
         this.liClass = "autocomplete-li";
         this.data = [];
         this.order = order;
+        this.highlight = highlight;
         this._init();
     }
 
     _init = () => {
-            //retire l'autocompletion du navigateur
-            this.input.setAttribute('autocomplete','off');
+        //retire l'autocompletion du navigateur
+        this.input.setAttribute('autocomplete', 'off');
 
-            //Creation du li et ajout d'une classe
-            let li = document.createElement('li');
-            li.className = this.liClass;
-            //Réglage de la largeur du la liste par rapport a la taille de l'input
-            li.style.width = getComputedStyle(this.input).width;
+        //Creation du li et ajout d'une classe
+        let li = document.createElement('li');
+        li.className = this.liClass;
+        //Réglage de la largeur du la liste par rapport a la taille de l'input
+        li.style.width = getComputedStyle(this.input).width;
 
-            //Evenement quand on écris dans l'input
-            this._addInputEvent(this.input, li);
+        //Evenement quand on écris dans l'input
+        this._addInputEvent(this.input, li);
     };
 
     _addInputEvent = (input, li) => {
@@ -33,7 +34,7 @@ class autocomplete {
                 this._orderData(data);
                 data.forEach((item) => {
                     let ul = document.createElement('ul');
-                    ul.innerText = item;
+                    ul.innerHTML = this._textRow(item);
                     this._addClickEventOnRow(ul, input);
                     li.append(ul);
                     input.after(li);
@@ -55,13 +56,18 @@ class autocomplete {
 
     _orderData = (data) => {
         if (this.order === 'ASC') {
-            console.log('test');
             return data.sort();
         } else if (this.order === 'DESC') {
             return data.sort().reverse();
         }
 
         return data;
+    };
+
+    _textRow = (text) => {
+        var pattern = new RegExp(this.input.value, 'i');
+        return this.highlight ? text.toLowerCase().replace(pattern,
+            '<span>' + this.input.value.toLowerCase() + '</span>') : text.toLowerCase();
     };
 
     setData = (data) => {
@@ -74,9 +80,13 @@ class autocomplete {
 
     setFilter = (filter) => {
         this.filter = filter;
-    }
+    };
 
     setOrder = (order) => {
         this.order = order;
+    };
+
+    setHighLight = (highlight) => {
+        this.highlight = highlight;
     }
 }
